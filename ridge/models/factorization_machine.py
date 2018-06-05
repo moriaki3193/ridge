0bcc881990ae4ab44b3d8f95b1aa2224449cc934
moriaki3193 <moriaki3193@gmail.com>
2018-06-06 01:32:18 +0900

    modify L2 Regularized FM

--- ridge/models/factorization_machine.py
+++ ridge/models/factorization_machine.py
@@ -8,17 +8,20 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class FacMac:
+    """2-way Factorization Machine (Rendle2010).
+    """
     
     def __init__(self):
-        self.epoch = None
         self.k = None
         self.b = None
         self.w = None
         self.V = None
+        self.l2 = None
         self.eta = None
 
-    def _initialize_params(self, n_features, k):
+    def _initialize_params(self, n_features, k, l2):
         self.k = k
+        self.l2 = l2
         self.b = 0.0
         self.w = np.zeros(n_features)
         # self.V = np.random.uniform(high=np.sqrt(k), size=(n_features, k))
@@ -28,16 +31,17 @@ class FacMac:
         """Update parameters using Stochastic Gradient Descent method.
         """
         nonzero_ind = np.nonzero(x)[0]
-        self.b = self.b + 2 * self.eta * residue
-        self.w = self.w + 2 * self.eta * residue * x
+        self.b = self.b + self.eta * (residue - self.l2 * self.b)
+        self.w = self.w + self.eta * (residue * x - self.l2 * self.w)
         V_cur = self.V
+        self.V -= self.l2 * self.V  # L2 Regularize in advance
         for f in range(self.k):
             shared_term = np.sum([V_cur[j, f] * x[j] for j in nonzero_ind])
             for i in nonzero_ind:
                 x_i = x[i]
                 V_if = V_cur[i, f]
                 partial = x_i * shared_term - V_if * np.power(x_i, 2)
-                self.V[i, f] = V_if + 2 * self.eta * residue * partial
+                self.V[i, f] = V_if + self.eta * residue * partial
 
     def predict_one(self, x):
         """A prediction with a given feature vector.
@@ -58,15 +62,39 @@ class FacMac:
             pairwise_scores.append(sum_squared - squared_sum)
         return self.b + pointwise_score + 0.5 * np.sum(pairwise_scores)
 
-    def fit(self, X_train, y_train, k=8, eta=1e-3, n_iter=1000):
+    def predict(self, X_test):
+        """A prediction with given feature vectors.
+
+        Parameters
+        ----------
+        X_test : ndarray, whose shape is (n_tests, n_features).
+
+        Return
+        ------
+        y_pred : ndarray, whose shape is (n_tests, ).
+        """
+
+    def fit(self, X_train, y_train, k=8, l2=0.02, eta=1e-3, n_iter=1000):
         n_samples, n_features = X_train.shape
         self.eta = eta
-        self._initialize_params(n_features, k)
+        self._initialize_params(n_features, k, l2)
+        sample_indices = [i for i in range(n_samples)]
         for _epoch in tqdm(range(n_iter)):
-            self.epoch = _epoch  # TODO 外す
-            for m in range(n_samples):
+            np.random.shuffle(sample_indices)
+            for m in sample_indices:
                 features = X_train[m]
                 obs = y_train[m]
                 pred = self.predict_one(features)
                 self._update_params(features, obs - pred)
         return self
+
+
+class CDFacMac:
+    """2-way Combination-Dependent Factorization Machine (Saigusa2018).
+    """
+
+    def __init__(self):
+        self.k = None
+        self.b = None
+        self.w = None
+        self.V = None
---
+++ tests/test_fm.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import unittest
+import numpy as np
+from tqdm import tqdm
+from ridge.models import FacMac
+
+
+class TestFM(unittest.TestCase):
+
+    def test_fm(self):
+        # [START データセットの作成]
+        X = np.array([
+           #  Users  |     Movies     |    Movie Ratings   | Time | Last Movies Rated
+           # A  B  C | TI  NH  SW  ST | TI   NH   SW   ST  |      | TI  NH  SW  ST
+            [1, 0, 0,  1,  0,  0,  0,   0.3, 0.3, 0.3, 0,     13,   0,  0,  0,  0 ],
+            [1, 0, 0,  0,  1,  0,  0,   0.3, 0.3, 0.3, 0,     14,   1,  0,  0,  0 ],
+            [1, 0, 0,  0,  0,  1,  0,   0.3, 0.3, 0.3, 0,     16,   0,  1,  0,  0 ],
+            [0, 1, 0,  0,  0,  1,  0,   0,   0,   0.5, 0.5,   5,    0,  0,  0,  0 ],
+            [0, 1, 0,  0,  0,  0,  1,   0,   0,   0.5, 0.5,   8,    0,  0,  1,  0 ],
+            [0, 0, 1,  1,  0,  0,  0,   0.5, 0,   0.5, 0,     9,    0,  0,  0,  0 ],
+            [0, 0, 1,  0,  0,  1,  0,   0.5, 0,   0.5, 0,     12,   1,  0,  0,  0 ],
+            [1, 0, 0,  0,  0,  0,  1,   0.3, 0.3, 0.3, 0,     18,   0,  0,  1,  0 ],
+        ])
+        y = np.array([5, 3, 1, 4, 5, 1, 5])
+        X_train = X[0:5, :]
+        y_train = y[0:5]
+        X_test = X[5:7, :]
+        y_test = y[5:7]
+        # [END データセットの作成]
+
+        model = FacMac().fit(X_train, y_train, k=4, n_iter=1000)
+        print(f'pred: {model.predict_one(X_test[0, :])}')
+        print(f'obs : {y_test[0]}')
+        print(f'pred: {model.predict_one(X_test[1, :])}')
+        print(f'obs : {y_test[1]}')
+
+
+if __name__ == '__main__':
+    unittest.main()
