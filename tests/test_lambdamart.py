8e705ba7ec9539046ab7e4bf3218b434380255e2
moriaki3193 <moriaki3193@gmail.com>
2018-08-20 11:04:42 +0900

    add test suite of LambdaMART & MQ2007 dataset

---
+++ tests/test_lambdamart.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class TestLambdaMART(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_initialization(self):
+        pass
+
+    def test_fitting(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
